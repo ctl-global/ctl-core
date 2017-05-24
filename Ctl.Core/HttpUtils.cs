87dd0a8e3666c02d45bcf8174a7e65110a5948f6
Cory Nelson <cnelson@ctlglobalsolutions.com>
2017-05-24 14:29:08 -0500

    Add HttpUtils and UriUtils classes. Upgrade project to .NET 4.6 (for FormattableString). Upgrade solution to VS2017 (for C# 7)

---
+++ Ctl.Core/HttpUtils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ctl
+{
+    /// <summary>
+    /// Utility methods for working with HTTP.
+    /// </summary>
+    public static class HttpUtils
+    {
+        /// <summary>
+        /// Crates a basic authentication header value
+        /// </summary>
+        /// <param name="username">The user name to encode. Must not include the ':' character.</param>
+        /// <param name="password">The password to encode.</param>
+        /// <returns>An encoded string to use in basic auth.</returns>
+        public static string CreateBasicAuthValue(string username, string password)
+        {
+            if (username == null) throw new ArgumentNullException(nameof(username));
+            if (password == null) throw new ArgumentNullException(nameof(password));
+            if (username.IndexOf(':') != -1) throw new ArgumentException($"{nameof(username)} must not contain the ':' character.");
+
+            return Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
+        }
+
+        /// <summary>
+        /// Crates a basic authentication header value
+        /// </summary>
+        /// <param name="credential">The credential to encode.</param>
+        /// <returns>An encoded string to use in basic auth.</returns>
+        public static string CreateBasicAuthValue(NetworkCredential credential)
+        {
+            if (credential == null) throw new ArgumentNullException(nameof(credential));
+            if (credential.Domain != null) throw new ArgumentException($"{nameof(credential)} must not specify a domain.");
+
+            return CreateBasicAuthValue(credential.UserName, credential.Password);
+        }
+    }
+}
---
+++ Ctl.Core/UriUtils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ctl
+{
+    /// <summary>
+    /// Utility methods for working with URIs.
+    /// </summary>
+    public static class UriUtils
+    {
+        /// <summary>
+        /// Escapes a formattable string so that each interpolated expression is URL-encoded.
+        /// </summary>
+        /// <param name="str">The string to format.</param>
+        /// <returns>A formatted string with each interpolated expression URL-encoded.</returns>
+        public static string Escape(FormattableString str)
+        {
+            return str.ToString(EscapedUriFormatProvider.Instance);
+        }
+
+        sealed class EscapedUriFormatProvider : IFormatProvider, ICustomFormatter
+        {
+            public static EscapedUriFormatProvider Instance { get; } = new EscapedUriFormatProvider();
+
+            public string Format(string format, object arg, IFormatProvider formatProvider)
+            {
+                switch (arg)
+                {
+                    case null:
+                        return string.Empty;
+                    case IFormattable fmt:
+                        return Uri.EscapeDataString(fmt.ToString(format, CultureInfo.InvariantCulture));
+                    case IConvertible cvt:
+                        return Uri.EscapeDataString(cvt.ToString(CultureInfo.InvariantCulture));
+                    default:
+                        return Uri.EscapeDataString(arg.ToString());
+                }
+            }
+
+            public object GetFormat(Type formatType)
+            {
+                return formatType == typeof(ICustomFormatter) ? this : null;
+            }
+        }
+    }
+}
